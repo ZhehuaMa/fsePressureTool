56d9a95c228b5628c855a1aebf9a538beb33f7b3
马哲骅 <zhehuama@deepglint.com>
2021-01-22 14:06:54 +0800

    reconstruct with Ezio

---
+++ fse/fse.go
@@ -0,0 +1,222 @@
+package fse
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"github.com/golang/glog"
+	"github.com/google/uuid"
+	"github.com/zhehuama/Ezio"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	httpStr   = "http://"
+	prefixUrl = "/x-api/v1/repositories/"
+)
+
+const (
+	Uid = "uuid"
+	Num = "num"
+)
+
+func generateDefaultEntityData(value *string) *EntityData {
+	entityData := &EntityData{
+		Type:    "feature",
+		Version: "2.7.3.0",
+		Value:   *value,
+	}
+	return entityData
+}
+
+func generateDefaultIncludeItem(data *EntityData) *IncludeItem {
+	includeItem := &IncludeItem{
+		Data: *data,
+	}
+	return includeItem
+}
+
+func generateEntityItem(data *EntityData, id, location string, time int64) *ObjectItem {
+	objItem := &ObjectItem{
+		Data:       *data,
+		ID:         id,
+		LocationID: location,
+		Time:       time,
+	}
+	return objItem
+}
+
+func generateDefaultSearchBody(includeItem *IncludeItem) *SearchBody {
+	searchBody := &SearchBody{
+		Type:             "face",
+		Include:          []IncludeItem{*includeItem},
+		IncludeThreshold: 0,
+		MaxCandidates:    3,
+	}
+	return searchBody
+}
+
+func GenerateRandomFeature(featureLength int) []float32 {
+	feature := make([]float32, featureLength)
+	var sum float32 = 0
+	rand.Seed(time.Now().UnixNano())
+	for i := range feature {
+		feature[i] = rand.Float32()
+		sum += feature[i] * feature[i]
+	}
+	sum = float32(math.Sqrt(float64(sum)))
+	for i := range feature {
+		feature[i] /= sum
+	}
+	return feature
+}
+
+func generateCompareObject(id string, data *EntityData) *CompareObject {
+	returnObjectItem := &CompareObject{
+		ID:   id,
+		Data: *data,
+	}
+	return returnObjectItem
+}
+
+func generateCompareBody(mObjects, nObjects []CompareObject) *CompareBody {
+	retCompareBody := &CompareBody{
+		Type:      "face",
+		Threshold: 0,
+		MObjects:  mObjects,
+		NObjects:  nObjects,
+	}
+	return retCompareBody
+}
+
+func EncodeFeature(feature []float32) *string {
+	buffer := new(bytes.Buffer)
+	err := binary.Write(buffer, binary.LittleEndian, feature)
+	if err != nil {
+		glog.Errorf("binary.Write failed: %s\n", err.Error())
+	}
+
+	featureBytes := make([]byte, len(feature)*4)
+	if n, err := buffer.Read(featureBytes); err != nil {
+		glog.Errorf("buffer.Read failed: %s\n", err.Error())
+		glog.Errorf("buffer.Read number: %d\n", n)
+	}
+
+	encodedString := base64.StdEncoding.EncodeToString(featureBytes)
+	return &encodedString
+}
+
+type task interface {
+	run(int64)
+}
+
+func (t SearchTask) run(featureNum int64) {
+	t.url = httpStr + t.IPPort + prefixUrl + "search"
+	var i int64 = 0
+	for ; i < featureNum; i++ {
+		feature := GenerateRandomFeature(t.FeatureLength)
+		encodedString := EncodeFeature(feature)
+
+		entityData := generateDefaultEntityData(encodedString)
+		includeItem := generateDefaultIncludeItem(entityData)
+		searchBody := generateDefaultSearchBody(includeItem)
+		searchBody.MaxCandidates = t.MaxCandidates
+
+		for _, repo := range t.Repositories {
+			searchBody.Repositories = append(searchBody.Repositories, repo)
+		}
+
+		body, err := json.Marshal(*searchBody)
+		if err != nil {
+			glog.Errorf("Fail to marshal json: %s\n", err.Error())
+			continue
+		}
+
+		request, _ := http.NewRequest("POST", t.url, bytes.NewReader(body))
+		httpTask := &Ezio.Task{Request: request}
+		Ezio.Append(httpTask)
+	}
+}
+
+func (t CompareTask) run(featureNum int64) {
+	t.url = httpStr + t.IPPort + prefixUrl + "compare"
+	var i int64 = 0
+	for ; i < featureNum; i++ {
+		feature1 := GenerateRandomFeature(t.FeatureLength)
+		encodedString1 := EncodeFeature(feature1)
+		feature2 := GenerateRandomFeature(t.FeatureLength)
+		encodedString2 := EncodeFeature(feature2)
+
+		entityData1 := generateDefaultEntityData(encodedString1)
+		entityData2 := generateDefaultEntityData(encodedString2)
+
+		mObjects := make([]CompareObject, 0, 1)
+		nObjects := make([]CompareObject, 0, 1)
+		mObjects = append(mObjects, *generateCompareObject("m-id-1", entityData1))
+		nObjects = append(nObjects, *generateCompareObject("n-id-1", entityData2))
+		compareBody := generateCompareBody(mObjects, nObjects)
+
+		body, err := json.Marshal(*compareBody)
+		if err != nil {
+			glog.Errorf("Fail to marshal json: %s\n", err.Error())
+			continue
+		}
+
+		request, _ := http.NewRequest("POST", t.url, bytes.NewReader(body))
+		httpTask := &Ezio.Task{Request: request}
+		Ezio.Append(httpTask)
+	}
+}
+
+func setEntityTimeLocation(item *ObjectItem, option *TimeLocationOption, num, totalFeatureNum int64) {
+	item.LocationID = strconv.Itoa(int(num % int64(option.LocationNum)))
+
+	start := time.Duration(option.StartTime) * time.Millisecond
+	end := time.Duration(option.EndTime) * time.Millisecond
+	timeRange := end - start
+	if timeRange <= 0 {
+		item.Time = 0
+		return
+	}
+
+	timeStep := timeRange / time.Duration(totalFeatureNum) / time.Millisecond
+	item.Time = num*int64(timeStep) + int64(timeStep)/2
+}
+
+func (t EntityTask) run(featureNum int64) {
+	t.url = httpStr + t.IPPort + prefixUrl + t.RepoName + "/entities"
+	var i int64 = 0
+	for ; i < featureNum; i++ {
+		feature := GenerateRandomFeature(t.FeatureLength)
+		encodedString := EncodeFeature(feature)
+		entityData := generateDefaultEntityData(encodedString)
+		var id string
+		if t.IdType == Uid {
+			id = uuid.New().String()
+		} else {
+			id = strconv.Itoa(int(i))
+		}
+		item := generateEntityItem(entityData, id, "0", 0)
+		setEntityTimeLocation(item, &t.Option, i, featureNum)
+		body, err := json.Marshal(*item)
+		if err != nil {
+			glog.Errorf("Fail to marshal json: %s\n", err.Error())
+			continue
+		}
+
+		request, _ := http.NewRequest("POST", t.url, bytes.NewReader(body))
+		httpTask := &Ezio.Task{Request: request}
+		Ezio.Append(httpTask)
+	}
+}
+
+func (frame *Frame) RunTask(maxCount int64) {
+	Ezio.Run()
+	frame.Task.run(maxCount)
+	Ezio.Stop()
+}
--- main.go
+++ main.go
@@ -1,105 +1,101 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/zhehuama/fsePressureTool/tool"
-    "os"
-    "strings"
+	"flag"
+	"github.com/golang/glog"
+	"github.com/zhehuama/fsePressureTool/fse"
+	"os"
+	"strings"
 )
 
 const (
-    entityTask  = "entityTask"
-    searchTask  = "searchTask"
-    compareTask = "compareTask"
+	entityTask  = "entityTask"
+	searchTask  = "searchTask"
+	compareTask = "compareTask"
 )
 
 var (
-    addr        string
-    repo        string
-    taskType    string
-    idType      string
-    startTimeMs int64
-    endTimeMs   int64
-    maxCount    int64
-    topk        int
-    featLen     int
-    LocNum      int
-    qps         int
-    threadNum   int
-    repos       []string
+	addr        string
+	repo        string
+	taskType    string
+	idType      string
+	startTimeMs int64
+	endTimeMs   int64
+	maxCount    int64
+	topk        int
+	featLen     int
+	LocNum      int
+	repos       []string
 )
 
-func initFlags() {
-    flag.StringVar(&addr, "addr", "127.0.0.1:30800", "FSE address, {IP}:{PORT}")
-    flag.StringVar(&repo, "repo", "repo", "Repo name")
-    flag.StringVar(&taskType, "type", entityTask,
-        "Task type: 'entityTask' for adding features, 'searchTask' for searching features, 'compareTask' for comparing task")
-    flag.StringVar(&idType, "id", tool.Uid, "ID type: 'uuid' for UUID, 'num' for number sequence, starting from 0")
-    flag.Int64Var(&startTimeMs, "st", 0, "The start time in millisecond, only used in 'entityTask' type")
-    flag.Int64Var(&endTimeMs, "et", 0, "The end time in millisecond, only used in 'entityTask' type")
-    flag.IntVar(&topk, "topk", 3, "Top K")
-    flag.IntVar(&featLen, "len", 384, "The length of feature")
-    flag.IntVar(&LocNum, "loc", 1, "The number of locations, only used in 'entityTask' type")
-    flag.IntVar(&qps, "qps", 1, "QPS")
-    flag.IntVar(&threadNum, "t", 1, "The number of threads")
-    flag.Int64Var(&maxCount, "max", 10, "Maximum times the task is executed")
+func initFlag() {
+	flag.StringVar(&addr, "addr", "127.0.0.1:30800", "FSE address, {IP}:{PORT}")
+	flag.StringVar(&repo, "repo", "repo", "Repo names, split by comma, ie. 'repo1,repo2'")
+	flag.StringVar(&taskType, "type", entityTask,
+		"Task type: 'entityTask' for adding features, 'searchTask' for searching features, 'compareTask' for comparing task")
+	flag.StringVar(&idType, "id", fse.Uid, "ID type: 'uuid' for UUID, 'num' for number sequence, starting from 0")
+	flag.Int64Var(&startTimeMs, "st", 0, "The start time in millisecond, only used in 'entityTask' type")
+	flag.Int64Var(&endTimeMs, "et", 0, "The end time in millisecond, only used in 'entityTask' type")
+	flag.IntVar(&topk, "topk", 3, "Top K")
+	flag.IntVar(&featLen, "len", 384, "The length of feature")
+	flag.IntVar(&LocNum, "loc", 1, "The number of locations, only used in 'entityTask' type")
+	flag.Int64Var(&maxCount, "max", 10, "Maximum times the task is executed")
 
-    flag.Parse()
+	flag.Parse()
 
-    if taskType != entityTask && taskType != searchTask && taskType != compareTask {
-        fmt.Fprintln(os.Stderr, "Unknown task type", taskType)
-        os.Exit(-1)
-    }
-    if idType != tool.Uid && idType != tool.Num {
-        fmt.Fprintln(os.Stderr, "Unknown ID type", idType)
-        os.Exit(-1)
-    }
-    repos = strings.Split(repo, ",")
+	if taskType != entityTask && taskType != searchTask && taskType != compareTask {
+		glog.Errorln("Unknown task type", taskType)
+		os.Exit(-1)
+	}
+	if idType != fse.Uid && idType != fse.Num {
+		glog.Errorln("Unknown ID type", idType)
+		os.Exit(-1)
+	}
+	repos = strings.Split(repo, ",")
 }
 
 func main() {
-    initFlags()
+	initFlag()
 
-    switch taskType {
-    case entityTask:
-        if len(repos) != 1 {
-            fmt.Fprintln(os.Stderr, "Entity task only support one repo name")
-            os.Exit(-1)
-        }
-        option := tool.TimeLocationOption{
-            StartTime:   startTimeMs,
-            EndTime:     endTimeMs,
-            LocationNum: LocNum,
-        }
-        task := tool.EntityTask{
-            IPPort:        addr,
-            RepoName:      repo,
-            FeatureLength: featLen,
-            IdType:        idType,
-            Option:        option,
-        }
-        frame := tool.Frame{Task: task}
-        frame.RunTask(qps, maxCount, threadNum)
-    case searchTask:
-        if len(repos) == 0 {
-            fmt.Fprintln(os.Stderr, "Empty repos")
-            os.Exit(-1)
-        }
-        task := tool.SearchTask{
-            IPPort:        addr,
-            MaxCandidates: topk,
-            FeatureLength: featLen,
-        }
-        task.Repositories = repos
-        frame := tool.Frame{Task: task}
-        frame.RunTask(qps, maxCount, threadNum)
-    case compareTask:
-        task := tool.CompareTask{
-            IPPort:        addr,
-            FeatureLength: featLen,
-        }
-        frame := tool.Frame{Task: task}
-        frame.RunTask(qps, maxCount, threadNum)
-    }
+	switch taskType {
+	case entityTask:
+		if len(repos) != 1 {
+			glog.Errorln("Entity task only support one repo name")
+			os.Exit(-1)
+		}
+		option := fse.TimeLocationOption{
+			StartTime:   startTimeMs,
+			EndTime:     endTimeMs,
+			LocationNum: LocNum,
+		}
+		task := fse.EntityTask{
+			IPPort:        addr,
+			RepoName:      repo,
+			FeatureLength: featLen,
+			IdType:        idType,
+			Option:        option,
+		}
+		frame := fse.Frame{Task: task}
+		frame.RunTask(maxCount)
+	case searchTask:
+		if len(repos) == 0 {
+			glog.Errorln("Empty repos")
+			os.Exit(-1)
+		}
+		task := fse.SearchTask{
+			IPPort:        addr,
+			MaxCandidates: topk,
+			FeatureLength: featLen,
+		}
+		task.Repositories = repos
+		frame := fse.Frame{Task: task}
+		frame.RunTask(maxCount)
+	case compareTask:
+		task := fse.CompareTask{
+			IPPort:        addr,
+			FeatureLength: featLen,
+		}
+		frame := fse.Frame{Task: task}
+		frame.RunTask(maxCount)
+	}
 }
